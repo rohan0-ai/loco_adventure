f0a0f497597527de932a536449030644243a7069
rohan0-ai <rohan.gupta.2898@gmail.com>
2026-06-22 00:42:34 +0530

    Fix Django storages config

--- loco_adventure/loco_adventure/settings.py
+++ loco_adventure/loco_adventure/settings.py
@@ -74,6 +74,9 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STORAGES = {
+    "default": {
+        "BACKEND": "django.core.files.storage.FileSystemStorage",
+    },
     "staticfiles": {
         "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
     },
